6c3d581f7de56c799b6d13f1b21d88ea2c2af95d
Sabith T H <sabith.haneef@target.com>
2018-08-18 17:11:24 +0530

    added event handlers to display questions

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,15 +10,25 @@ class Quiz extends Component {
         this.state = { quiz_position: 1 };
     }
 
+    showNextQuestion() {
+        this.setState({
+            quiz_position: this.state.quiz_position + 1
+        })
+    }
+
     render() {
         const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length);
         return (
             <div>
                 <div className="QuizQuestion">
                     {
-                        isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} /> 
+                        isQuizEnd ? 
+                            <QuizEnd /> : 
+                            <QuizQuestion 
+                                quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} 
+                                showNextQuestionHandler={this.showNextQuestion.bind(this)} 
+                            />
                     }
-
                 </div>
             </div>
         )
